ac5c5ccd72c9d7c1034bf04c118e2daac078fe72
hkgithub22 <91311832+hkgithub22@users.noreply.github.com>
2021-11-27 13:49:14 +0530

    Add files via upload

---
+++ QUIZ/js/c.js
@@ -0,0 +1,11 @@
+
+
+var start=document.querySelector(".start_btn").onclick = function () {
+    location.href = "Quiz.html";
+};
+var start=document.querySelector(".start_btn1").onclick = function () {
+    location.href = "quiz3.html";
+};
+var start=document.querySelector(".start_btn2").onclick = function () {
+    location.href = "QUIZ4.html";
+};
