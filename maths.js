395ab7b9e2d5125905c2b148398a7de5980ff4ab
hkgithub22 <91311832+hkgithub22@users.noreply.github.com>
2021-11-27 15:16:30 +0530

    Add files via upload

---
+++ maths.js
@@ -0,0 +1,119 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is three fifth of 100??",
+    answer: "60",
+    options: [
+      "3",
+      "5",
+      "20",
+      "60"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is 7% equal to ",
+    answer: "0.07",
+    options: [
+      "0.07",
+      "7",
+      "0.007",
+      "0.7"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many years are there in a decade?",
+    answer: "10",
+    options: [
+      "9",
+      "10",
+      "33",
+      "29"
+    ]
+  },
+    {
+    numb: 4,
+    question: " In a century how many months are there?",
+    answer: "1200",
+    options: [
+      "1200",
+
+      "12",
+      
+      "1200",
+      
+      "120"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the value of x in the equation 3x – 15 – 6 = 0 ?",
+    answer: "7",
+    options: [
+      "5",
+      "-9",
+      "9",
+      "7"
+    ]
+  },
+  {
+    numb: 6,
+    question: "|–4| + |4| – 4 + 4 = ?",
+    answer: "8",
+    options: [
+      "4",
+      "16",
+      "8",
+      "-9"
+    ]
+    
+  },
+  {
+    numb: 7,
+    question: "An angle whose value is ____, is called complete angle.",
+    answer: "360°",
+    options: [
+      "180°",
+      "240°",
+      "360°",
+      "none of the above"
+    ]
+    
+  },
+  {
+    numb: 8,
+    question: "An amount of Rs. 10,000 becomes Rs. 20,736 in 2 years. If the rate of interest is compounded half yearly, what is the annual rate of interest?",
+    answer: "40%",
+    options: [
+      "40%",
+      "25%",
+      "30%",
+      "-25%"
+    ]
+    
+  },
+  {
+    numb: 9,
+    question: "4950/6 + 112 x 1.75 = ? x 2",
+    answer: "510.5",
+    options: [
+      "510.5",
+      "505.2",
+      "515.5",
+      "none of the above"
+    ]
+    
+  },
+  {
+    numb: 10,
+    question: "If Px = Qy = Rz  and Q/P = R/Q, then 2z/(x+z) = ?",
+    answer: "y/x",
+    options: [
+      "y/x",
+      "x/y",
+      "y/z",
+      "z/y"
+    ]
+    
+  }];
---
+++ science.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Oxidation is a process which involves?",
+    answer: "addition of oxygen",
+    options: [
+      "removal of oxygen",
+      "removal of hydrogen",
+      "addition of hydrogen",
+      "addition of oxygen"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is an endothermic process? ",
+    answer: "Sublimation of dry ice",
+    options: [
+      " Dilution of sulphuric acid",
+      "Sublimation of dry ice",
+      "Condensation of water vapours",
+      "Respiration in human beings"
+    ]
+  },
+    {
+    numb: 3,
+    question: ". What type of chemical reactions take place when electricity is passed through water?",
+    answer: "Decomposition",
+    options: [
+      "Decomposition",
+      "Combination",
+      "Double displacement",
+      " Displacement"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Which of the following gases can be used for storage?",
+    answer: " Helium or Nitrogen",
+    options: [
+      " Helium or Nitrogen",
+
+      " Nitrogen or Oxygen",
+      
+      " Carbon dioxide or Oxygen",
+      
+      "Carbon dioxide or Helium"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following reactions will not take place ?",
+    answer: "Zn + MgSO4 → ZnSO4 + Mg",
+    options: [
+      "Zn + CuSO4 → ZnSO3 + Cu",
+      "Zn + MgSO4 → ZnSO4 + Mg",
+      " 2KBr + Cl2 → KCl + Br2",
+      " Mg + FeSO4 → MgSO4 + Fe"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following involves combination of two elements ?",
+    answer: "N2(g) + 3H2(g) → 2NH3(g)",
+    options: [
+      "N2(g) + 3H2(g) → 2NH3(g)",
+      " CaO(s) + CO2(g) → CaCO3(g)",
+      "2SO2(g) + O2(g) → 2SO3f(g)",
+      "NH3(g) + HCl(g) → NH4Cl(s)"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The brown gas evolved on heating of copper nitrate is",
+    answer: "NO2",
+    options: [
+      " O2",
+      "N2",
+      "NO2",
+      "NO"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following mirror is used by a dentist to examine a small cavity?",
+    answer: "Concave mirror",
+    options: [
+      "Convex mirror",
+      " Plane mirror",
+      "Concave mirror",
+      "Combination of convex and concave mirror"
+    ]
+  },
+  {
+    numb: 9,
+    question: "As light travels from a rarer to a denser medium it will have",
+    answer: " both (b) and (c)",
+    options: [
+      "increased velocity",
+      "decreased velocity",
+      "decreased wavelength",
+      " both (b) and (c)"
+    ]
+  },{
+    numb: 10,
+    question: "The angle of incidence i and refraction r are equal in a transparent slab when the value of i is",
+    answer: "0°",
+    options: [
+      "0°",
+      "45°",
+      "90°",
+      "depend on the material of the slab"
+    ]
+  }
+  
+];
