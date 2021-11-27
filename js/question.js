186bc03e059950c00c82a4020cc0335289d91d77
hkgithub22 <91311832+hkgithub22@users.noreply.github.com>
2021-11-27 15:11:33 +0530

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Warli painting is indigenous to which State?",
+    answer: "Maharashtra",
+    options: [
+      "Maharashtra",
+      "Odisha",
+      "Rajasthan",
+      "Gujarat"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Japanese art of paper folding is known as ",
+    answer: "Origami",
+    options: [
+      "Haiku",
+      "Origami",
+      "Ikebana",
+      "Karaoke"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Raja Ravi Varma was a famous –",
+    answer: "Painter",
+    options: [
+      "Musician",
+      "Singer",
+      "Physician",
+      "Painter"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is/are the example of cave paintings?",
+    answer: "All of the above",
+    options: [
+      "Ajanta Caves",
+
+      "Armamalai Cave",
+      
+      "Badami Cave Temples",
+      
+      "All of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The colors known as primary colors are",
+    answer: "red, green and blue",
+    options: [
+      "red and green",
+      "red, yellow and green",
+      "red, blue and orange",
+      "red, green and blue"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who of the following is/was not a painter?",
+    answer: "Ramkinkar Baiz",
+    options: [
+      "Tyeb Mehta",
+      "Ramkinkar Baiz",
+      "Syed Haider Raza",
+      "Jamini Roy"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What was the subject of the earliest known paintings?",
+    answer: "Animals",
+    options: [
+      "Animals",
+      "Sports",
+      " Flowers",
+      "Landscapes"
+    ]
+  },
+  {
+    numb: 8,
+    question: " ‘Madhubani’ a style of folk paintings, is popular in which of the following states in India?",
+    answer: "Bihar",
+    options: [
+      "Rajasthan",
+      "West Bengal",
+      "Bihar",
+      "Madhya Pradesh"
+    ]
+  },
+  {
+    numb: 9,
+    question: "The technique of mural painting executed upon freshly laid lime plaster is known as ",
+    answer: "Fresco",
+    options: [
+      "Fresco",
+      "Gouache",
+      "Tempera",
+      "Cubism"
+    ]
+  },{
+    numb: 10,
+    question: "Blue is an example of this element:",
+    answer: "color",
+    options: [
+      "texture",
+      "color",
+      "contrast",
+      "value"
+    ]
+  }
+  
+];
